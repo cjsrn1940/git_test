4e611f23eb4167b7bb36ecbe9c36400a508b06e2
차예진 <@DESKTOP-57BI62I>
2021-05-25 11:43:12 +0900

    5/2 코딩

--- src/com/javaex/git/Ex02.java
+++ src/com/javaex/git/Ex02.java
@@ -1,9 +1,8 @@
 package com.javaex.git;
 
-public class Ex01 {
+public class Ex02 {
 
 	public static void main(String[] args) {
-		System.out.println("5/1 코딩");
 		System.out.println("5/2 코딩");
 
 	}
